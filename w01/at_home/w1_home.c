df22c81eda4ac50615c11d94f5b58c4c6ce5bd41
Daniel Paul Grech Pereira <13129650+pereiradaniel@users.noreply.github.com>
2019-10-14 11:15:48 -0400

    w01 initial commit.
    w01 assignments added.

---
+++ w01/at_home/w1_home.c
@@ -0,0 +1,10 @@
+//  Daniel Paul Grech Pereira
+//  dppereira@myseneca.ca
+//  037747078
+
+#include <stdio.h>
+int main(void)
+{
+	printf("********************************\n*** Welcome to C Programming ***\n********************************");
+	return 0;
+}
